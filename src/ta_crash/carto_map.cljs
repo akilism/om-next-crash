453541b8d534dc8cfd001b76a43a0847b9b5e8a9
Akil Harris <akil.harris@gmail.com>
2016-02-08 22:02:27 -0500

    dead code elimination

--- src/ta_crash/carto_map.cljs
+++ src/ta_crash/carto_map.cljs
@@ -11,34 +11,6 @@
   [error]
   (pprint/pprint error))
 
-(defn get-geo-table [type]
-  (condp = type
-    :borough "nyc_borough"
-    :city-council "nyc_city_council"
-    :community-board "nyc_community_board"
-    :neighborhood "nyc_neighborhood"
-    :precinct "nyc_nypd_precinct"
-    :zip-code "nyc_zip_codes"))
-
-(defn get-geo-identifier [identifier]
-  (if (string? identifier)
-    (str "'" identifier "'")
-    identifier))
-
-(defn set-map-bounds
-  [query map-el params]
-  ;(println "bounds: " params)
-  (let [c-sql (js/cartodb.SQL. (clj->js {:user "akilism"}))
-        query-str (query params)]
-    (-> (.getBounds c-sql query-str)
-        (.done #(.fitBounds map-el %)))))
-
-(defn set-map-query
-  [query layer params]
-  (println "query: " params)
-  (let [query-str (query params)]
-    (.setSQL layer (query params))))
-
 (defn set-map
   [query layer map-el]
   (let [c-sql (js/cartodb.SQL. (clj->js {:user "akilism"}))]
@@ -57,28 +29,8 @@
   (map-new-parameters
     [this vis props]
     (let [data-layer (nth (.getLayers vis) 1)
-         sub-layer (.getSubLayer data-layer 0)
-         ;{:keys [start-date end-date active-area active-stat]} props
-         ;{:keys [area-type identifier]} active-area
-         ;params {:end-date end-date :start-date start-date}
-         ]
-     (set-map (queries/get-query :crashes props) sub-layer (.getNativeMap vis))
-     ;(cond
-     ;  (and (or (= "citywide" (:identifier active-area)) (empty? active-area)) (nil? active-stat))
-     ;   (set-map (queries/get-query :crashes-date [end-date start-date]) sub-layer (.getNativeMap vis))
-     ;  (and (or (= "citywide" (:identifier active-area)) (empty? active-area)) (complement nil? active-stat))
-     ;   (set-map (queries/get-query :crashes-date-filtered [end-date start-date active-stat]) sub-layer (.getNativeMap vis))
-     ;  (and (not (= "citywide" (:identifier active-area))) (nil? active-stat))
-     ;   (set-map (queries/get-query :crashes-date-area [end-date start-date area-type identifier]) sub-layer (.getNativeMap vis))
-     ;  (and (not (= "citywide" (:identifier active-area))) (complement nil? active-stat))
-     ;   (set-map (queries/get-query :crashes-date-area-filtered [end-date start-date area-type identifier active-stat]) sub-layer (.getNativeMap vis)))
-     ;(if (or (empty? active-area) (= "citywide" (:identifier active-area)))
-     ;   (do (set-map-query queries/crashes-by-date sub-layer params)
-     ;       (set-map-bounds queries/crashes-by-date (.getNativeMap vis) params))
-     ;   (let [final-params (assoc params :geo-table (get-geo-table area-type) :identifier (get-geo-identifier identifier))]
-     ;     (set-map-query queries/crashes-by-date-area sub-layer final-params)
-     ;     (set-map-bounds queries/crashes-by-date-area (.getNativeMap vis) final-params)))
-     ))
+          sub-layer (.getSubLayer data-layer 0)]
+     (set-map (queries/get-query :crashes props) sub-layer (.getNativeMap vis))))
   (componentDidMount [this]
     (let [{:keys [text type]} (om/props this)
           viz "https://akilism.cartodb.com/api/v2/viz/fcdfe28c-b6de-11e5-849b-0e787de82d45/viz.json"]
